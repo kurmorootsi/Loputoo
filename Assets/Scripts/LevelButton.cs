9738be79ea738c260850d5f650b25939670c279d
kurmorootsi <kurmo.rootsi@hotmail.com>
2020-05-01 11:53:07 +0300

    Menüü ja leveli süsteemi arendus

---
+++ Assets/Scripts/LevelButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelButton : MonoBehaviour
+{
+	[SerializeField]
+	public int loadScene;
+
+	public int givenStars;
+
+	public GameObject[] lvlButtons;
+
+	private void Start()
+	{
+		Debug.Log("started level-" + loadScene);
+
+		givenStars = PlayerPrefs.GetInt("level_" + loadScene);
+
+		Debug.Log("stars given-" + givenStars);
+
+
+		for (int i = 0; i <= givenStars-1; i++)
+		{
+			lvlButtons[i].SetActive(true);
+		}
+	}
+
+	public void loadLevelScene()
+	{
+		SceneManager.LoadScene(loadScene);
+	}
+}
---
+++ Assets/Scripts/MoveToNextLevel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToNextLevel : MonoBehaviour
+{
+	public int nextSceneLoad;
+	public int currentScene;
+
+	[SerializeField]
+	public int givenStars;
+    
+    void Start()
+    {
+		currentScene = SceneManager.GetActiveScene().buildIndex;
+
+		nextSceneLoad = SceneManager.GetActiveScene().buildIndex + 1;
+	}
+
+	public void loadNextLevel()
+	{
+		PlayerPrefs.SetInt("level_"+currentScene, givenStars);
+
+		Debug.Log("pref set-level_" + currentScene + " stars-" + givenStars);
+
+		SceneManager.LoadScene(nextSceneLoad);
+
+		if (nextSceneLoad > PlayerPrefs.GetInt("levelAt"))
+		{
+			PlayerPrefs.SetInt("levelAt", nextSceneLoad);
+		}
+	}
+
+}
